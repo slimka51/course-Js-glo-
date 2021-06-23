46045ee81ebfb6e7752ddda599b21f479b21cf8a
Dmitriy <2cam@mail.ru>
2021-06-23 20:31:01 +0300

    добавление методов в объект

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ let budget,
     price
 
 function start() {
-    budget = +prompt("Ваш бюджет?");
+    budget = +prompt("Ваш бюджет?", '');
 
     while(isNaN(budget) || budget == "" || budget == null) {
-        budget = +prompt("Ваш бюджет?");
+        budget = +prompt("Ваш бюджет?", '');
     }
 
-    nameShop = prompt("Название вашего магазина?").toUpperCase();
+    nameShop = prompt("Название вашего магазина?", '').toUpperCase();
     time = 19;
 }
 
@@ -22,84 +22,88 @@ let mainList = {
     shopGoods: [],
     employers: {},
     open: false,
-    discount: true 
-}
+    discount: true,
+    shopItems: [],
+    chooseGoods: function chooseGoods() {
+            for (let i = 0; i < 5; i++) {
+        
+                let a = prompt("Какой тип товаров будем продавать?", '');
+        
+                if((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
+                    console.log('Все верно!');
+                    mainList.shopGoods[i] = a;
+                } else {
+                    i = i - 1;
+                }
+            }
+    },
+    
+    workTime: function workTime(time) {
+            if (time < 0) {
+                console.log('Такого просто не может быть');
+            } else if (time > 8 && time < 20) {
+                console.log('Время работать!');
+                mainList.open = true;
+                } else if (time < 24) {
+                    console.log('Уже слишком поздно!');
+                } else {
+                    console.log('В сутках только 24 часа!');
+                }
+    },
+    
+    dayBudget: function dayBudget() {
+            alert("Ежедневный бюджет " + mainList.cash / 30);
+    },
 
-function chooseGoods() {
-    for (let i = 0; i < 5; i++) {
+    sale: function sale(price) {
+            if (mainList.discount == true) {
+                price = price * 80/100;
+            }
+    },
 
-        let a = prompt("Какой тип товаров будем продавать?");
+    employers: function employers() {
+            for (let i = 0; i < 4; i++) {
+                let name = prompt("Введите имя сотрудника", ''); 
+        
+                if((typeof(name)) === 'string' && (typeof(name)) != null && name != '' && name.length < 50) {
+                    mainList.worker[i] = name;
+                } else {
+                    i = i - 1;
+                }
+                
+            }
+    },
+    chooseShopItems: function chooseShopItems() {
+        let items = prompt("Перечислите через запятую товары", "");
 
-        if((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
-            console.log('Все верно!');
-            mainList.shopGoods[i] = a;
+        if((typeof(items)) === 'string' && (typeof(items)) != null && items != ''){
+            mainList.shopItems = items.split(',');
+            mainList.shopItems.push(prompt("Подождите, еще ", ""));
+            mainList.shopItems.sort();
+            mainList.shopItems.forEach(function(index, i, arr) {
+                console.log("У нас вы можете купить: " + (i + 1) + ' ' + index);
+            }) 
         } else {
-            i = i - 1;
-        }
+           this.chooseShopItems();
+        }       
+
     }
 }
-
-// chooseGoods();
-
-
-/* let k = 0;
-while (k < 5) {
-    let name = prompt("Какой тип товаров будем продавать?");
-    mainList.shopGoods[k] = name;
-    k++;
-} */
-/* let k = 0;
-do {
-    let name = prompt("Какой тип товаров будем продавать?");
-    mainList.shopGoods[k] = name;
-    k++;
-} while (k < 5); */
-
-
-function workTime(time) {
-    if (time < 0) {
-        console.log('Такого просто не может быть');
-    } else if (time > 8 && time < 20) {
-        console.log('Время работать!');
-        } else if (time < 24) {
-            console.log('Уже слишком поздно!');
-        } else {
-            console.log('В сутках только 24 часа!');
-        }
-}
-
-workTime(-10);
-
-// alert("Ежедневный бюджет " + mainList.cash / 30);
 
 console.log(mainList);
 
-function sale(price) {
-    if (mainList.discount == true) {
-        price = price * 80/100;
-        console.log(price);
-    } else {
-        console.log(price);
-    }
+for(keys in mainList) {
+    console.log("Наш магазин включает в себя: " + keys);
 }
 
-sale(500);
 
 
-let worker = {};
 
-function employers() {
-    for (let i = 0; i < 4; i++) {
-        let name = prompt("Введите имя сотрудника"); 
 
-        if((typeof(name)) === 'string' && (typeof(name)) != null && name != '' && name.length < 50) {
-            worker[i] = name;
-        } else {
-            i = i - 1;
-        }
-        
-    }
-}
-employers();
 
-console.log(worker);
+
+
+
+
+
+
